4c185ab9ebb1da932f977e98e420a180e3ae3db4
RafaelNavon <raffi158@gmail.com>
2023-11-08 20:13:08 +0200

    Updated scraping method

--- bot.js
+++ bot.js
@@ -1,24 +1,20 @@
 require('dotenv').config();
-const TelegramBot = require('node-telegram-bot-api');
-const puppeteerExtra = require('puppeteer-extra');
-const Stealth = require('puppeteer-extra-plugin-stealth');
 const axios = require('axios');
+const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const zenRowsApiKey = process.env.ZENROWS_API_KEY; // Ensure this is set in your .env file
 const azureKey = process.env.AZURE_TRANSLATOR_KEY;
 const azureEndpoint = process.env.AZURE_TRANSLATOR_ENDPOINT;
-
-puppeteerExtra.use(Stealth());
 
 const bot = new TelegramBot(token, { polling: true });
 let userLanguage = 'en'; // default language
 
 bot.onText(/set language (.+)/, async (msg, match) => {
   const chatId = msg.chat.id;
-  const lang = match[1];
-  userLanguage = lang;
+  userLanguage = match[1];
 
-  let responseMessage = 'No problem';
+  let responseMessage = 'Language set to ' + userLanguage;
 
   if (userLanguage !== 'en') {
     responseMessage = await translateText(responseMessage, userLanguage);
@@ -29,7 +25,7 @@ bot.onText(/set language (.+)/, async (msg, match) => {
 
 bot.onText(/^(\d+)$/, async (msg, match) => {
   const chatId = msg.chat.id;
-  const jokeNumber = match[1];
+  const jokeNumber = parseInt(match[1], 10);
 
   if (jokeNumber < 1 || jokeNumber > 101) {
     bot.sendMessage(chatId, 'Please send a number between 1 and 101.');
@@ -38,8 +34,8 @@ bot.onText(/^(\d+)$/, async (msg, match) => {
 
   try {
     let joke = await scrapeJoke(jokeNumber);
-    if (!joke) {
-      bot.sendMessage(chatId, 'Failed to retrieve the joke. Please try another number.');
+    if (joke === "Joke number is out of range.") {
+      bot.sendMessage(chatId, joke);
       return;
     }
 
@@ -55,33 +51,42 @@ bot.onText(/^(\d+)$/, async (msg, match) => {
 });
 
 async function scrapeJoke(jokeNumber) {
-  const browser = await puppeteerExtra.launch({ headless: true, devtools: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-  const page = await browser.newPage();
-  const delay = duration => new Promise(res => setTimeout(res, duration));
+  // The parameters provided by ZenRows should be used here.
+  const url = 'https://parade.com/968666/parade/chuck-norris-jokes/';
+  const apiKey = zenRowsApiKey; // Replace with your actual ZenRows API key from your .env file
 
+  try {
+    const response = await axios({
+      url: 'https://api.zenrows.com/v1/',
+      method: 'GET',
+      params: {
+        'url': url,
+        'apikey': apiKey,
+        'js_render': 'true'
+      }
+    });
 
-  await page.setViewport({ width: 1366, height: 768 });
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36');
+    if (response.status !== 200) {
+      throw new Error(`Failed to retrieve jokes, status code: ${response.status}`);
+    }
 
-  const response = await page.goto('https://parade.com/968666/parade/chuck-norris-jokes/', { waitUntil: 'domcontentloaded' });
-  await delay(40000); // wait for 40 seconds to manually solve CAPTCHA
-  if (response.status() !== 200) {
-    throw new Error(`Failed to load page, status: ${response.status()}`);
+    // Process the returned HTML here to extract the jokes.
+    // The exact method will depend on the structure of the HTML.
+    // Assuming the HTML structure is similar to what we've seen before,
+    // we could use a library like Cheerio to parse it (Cheerio must be installed first with npm install cheerio):
+    const cheerio = require('cheerio');
+    const $ = cheerio.load(response.data);
+    const jokes = $('.m-detail--body > ol > li').toArray().map(item => $(item).text());
+    const joke = jokes[jokeNumber - 1];
+    return joke || "Joke number is out of range.";
+
+  } catch (error) {
+    console.error('Error during web scraping:', error.message);
+    return null; // In case of an error, return null for joke
   }
-
-  await page.waitForSelector('.m-detail--body > ol > li');
-
-  const joke = await page.evaluate((jokeNum) => {
-    const element = document.querySelector(`.m-detail--body > ol > li:nth-child(${jokeNum})`);
-    return element ? element.innerText.trim() : null;
-  }, jokeNumber);
-
-  await browser.close();
-
-  return joke;
 }
 
-async function translateText(joke, language) {
+async function translateText(text, language) {
   try {
     const response = await axios({
       baseURL: azureEndpoint,
@@ -97,7 +102,7 @@ async function translateText(joke, language) {
         'to': language,
       },
       data: [{
-        'text': joke
+        'text': text
       }],
       responseType: 'json',
     });
@@ -110,30 +115,3 @@ async function translateText(joke, language) {
 }
 
 console.log('Bot started...');
-
-
-
-
-
-
-
-
-
-// const axios = require('axios');
-// const cheerio = require('cheerio');
-
-// const URL = 'https://parade.com/968666/parade/chuck-norris-jokes/';
-
-// axios.get(URL).then(response => {
-//     const $ = cheerio.load(response.data);
-    
-//     // Extract jokes
-//     const jokesList = $('div.m-detail--body ol li');
-    
-//     jokesList.each((index, element) => {
-//         console.log(`${index + 1}. ${$(element).text().trim()}`);
-//     });
-
-// }).catch(error => {
-//     console.error("Error fetching the URL:", error);
-// });
